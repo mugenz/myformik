e862d81e563fed9db807515f255d37efe6410191
Manos M <markou_13@yahoo.com>
2019-04-02 00:07:13 +0300

    fron sandbox
    hi from sandbox

--- src/components/RenderUse.js
+++ src/components/RenderUse.js
@@ -1,40 +1,33 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Formik, Form, Field } from 'formik';
+import React from "react";
+import styled from "styled-components";
+import { Formik, Form, Field } from "formik";
 
 const StyledInput = styled.div`
-  color: ${(props) => props.theme.color};
-  background-color: ${(props) => props.theme.color2};
+  color: ${props => props.theme.color};
+  background-color: ${props => props.theme.color2};
 `;
 
 const RenderUse = () => (
   <div>
     <h1>My Form</h1>
     <Formik
-      initialValues={{ name: 'jared' }}
+      initialValues={{ name: "jare" }}
       onSubmit={(values, actions) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
           actions.setSubmitting(false);
         }, 1000);
-      }
-      }
+      }}
       handleChange={() => {
-        console.log('CHANGE')
-      }
-      }
+        console.log("CHANGE");
+      }}
       handleBlur={(values, actions) => {
-        console.log('BLur')
-      }
-      }
-      
+        console.log("BLur");
+      }}
       render={props => (
         <StyledInput>
-          <Form >
-            <Field
-              value={props.values.name}
-              name="name"
-            />
+          <Form>
+            <Field value={props.values.name} name="name" />
             <Field component="select" name="color">
               <option value="red">Red</option>
               <option value="green">Green</option>
@@ -47,6 +40,5 @@ const RenderUse = () => (
     />
   </div>
 );
-
 
 export default RenderUse;
