e27375069ee845c9eff13db79647efd339efc8b7
Manos M <markou_13@yahoo.com>
2019-04-03 12:13:45 +0300

    changes
    render use

--- src/components/RenderUse.js
+++ src/components/RenderUse.js
@@ -1,53 +1,48 @@
-import React, { useContext } from 'react';
-import styled from 'styled-components';
-import { Formik, Form, Field } from 'formik';
-import UserContext from './context/UserContext';
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { Formik, Form, Field } from "formik";
+import UserContext from "./context/UserContext";
 
 const StyledInput = styled.div`
   color: ${props => props.theme.color};
   background-color: ${props => props.theme.color2};
 `;
 
-
 const RenderUse = () => {
-  const data = useContext(UserContext)
+  const data = useContext(UserContext);
   return (
-  <div>
-    <h1>My Form</h1>
-    <Formik
-      initialValues={{ name: data.kostas }}
-      onSubmit={(values, actions) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }, 1000);
-      }}
-      handleChange={() => {
-        console.log("CHANGE");
-      }}
-      handleBlur={(values, actions) => {
-        console.log("BLur");
-      }}
-      render={props => (
-        <StyledInput>
-          <Form >
-            <Field
-              value={data.manos}
-              name="name"
-            />
-            <Field component="select" name="color">
-              <option value="red">Red</option>
-              <option value="green">Green</option>
-              <option value="blue">Blue</option>
-            </Field>
-            <button>Submit</button>
-          </Form>
-        </StyledInput>
-      )}
-    />
-  </div>
-)
-
+    <div>
+      <h1>My Form</h1>
+      <Formik
+        initialValues={{ name: data.kostas }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            actions.setSubmitting(false);
+          }, 1000);
+        }}
+        handleChange={() => {
+          console.log("CHANGE");
+        }}
+        handleBlur={(values, actions) => {
+          console.log("BLur");
+        }}
+        render={props => (
+          <StyledInput>
+            <Form>
+              <Field value={data.kostas} name="name" />
+              <Field component="select" name="color">
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+              </Field>
+              <button>Submit</button>
+            </Form>
+          </StyledInput>
+        )}
+      />
+    </div>
+  );
 };
 
 export default RenderUse;
